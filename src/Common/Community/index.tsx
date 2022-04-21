d8184851efef0f35937494c55880485fcdd55b9c
yinwai <yinwai@sjtu.edu.cn>
2022-04-21 21:09:35 +0800

    community

--- src/Common/Community/index.tsx
+++ src/Common/Community/index.tsx
@@ -2,12 +2,42 @@
  * @Author: yinwai
  * @Date:   2022-04-19 01:56:27
  * @Last Modified by:   yinwai
- * @Last Modified time: 2022-04-20 14:23:41
+ * @Last Modified time: 2022-04-21 21:07:55
  */
 
 import React from "react";
+import { List, Avatar, Image } from "antd-mobile";
+import { posts } from "./data";
+import Styles from './index.module.scss';
+
+export interface Post {
+	avatar: string,
+	name: string,
+	time: string,
+	content: string,
+	pics: string[]
+};
 
 const Community: React.FunctionComponent = () => {
-	return (<div>社区</div>);
-}
+	return (
+		<List className={Styles.root}>{
+			posts.map((item: Post, index: number) => (
+				<List.Item key={index} className="item" prefix={<Avatar src={item.avatar} className="avatar" />} title={(<div className="name">{item.name}</div>)} description={(<div className="time">{item.time}</div>)}>
+					<div className="content">
+						{item.content}
+					</div>
+					{(item.pics.length === 0) ? null : (
+						<div className="pictures">
+						{
+							item.pics.map((item: string, index: number) => (
+								<Image src={item} className="picture" />
+							))
+						}
+						</div>
+					)}
+				</List.Item>
+			))
+		}</List>
+	);
+};
 export default Community;
